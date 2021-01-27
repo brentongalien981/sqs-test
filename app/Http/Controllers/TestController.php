a6270399502b0c4f4cc2fcbc82fb564f4bf0a90c
brentongalien981 <brentongalien981@gmail.com>
2021-01-27 14:27:35 -0500

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -24,13 +24,13 @@ class TestController extends Controller
         $userData['email'] = $request->email;
         $userData['password'] = $request->password;
 
-        AddUser::dispatch($userData)->onQueue('QOrders.fifo');
+        AddUser::dispatch($userData)->onQueue('QOrders');
 
         return [
             'name' => $request->name,
             'email' => $request->email,
             'password' => $request->password,
-            'msg' => 'JOB: AddUser dispatched on queue: QOrders.fifo...'
+            'msg' => 'JOB: AddUser dispatched on queue: QOrders...'
         ];
     }
 
