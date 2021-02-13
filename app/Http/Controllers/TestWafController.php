df5f917a5cb31bb5c3d153685e82f26c2193150e
brentongalien981 <brentongalien981@gmail.com>
2021-02-13 03:32:15 -0500

    Update TestWafController.php

--- app/Http/Controllers/TestWafController.php
+++ app/Http/Controllers/TestWafController.php
@@ -11,6 +11,8 @@ class TestWafController extends Controller
             'msg' => 'In CLASS: TestWafController, METHOD: test()',
             'r->ip()' => $r->ip(),
             'SERVER[HTTP_HOST]' => $_SERVER['HTTP_HOST'],
+            'SERVER[HTTP_USER_AGENT]' => $_SERVER['HTTP_USER_AGENT'],
+            'SERVER[REMOTE_ADDR]' => $_SERVER['REMOTE_ADDR'],
         ];
     }
 }
