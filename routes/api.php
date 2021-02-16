602a06f7d00c3ea6096886204eb3f0031443551e
brentongalien981 <brentongalien981@gmail.com>
2021-02-16 04:49:20 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Route::get('/test-api-route', function () {
+    return [
+        'msg' => 'This is test-api-route'
+    ];
+});
+
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
